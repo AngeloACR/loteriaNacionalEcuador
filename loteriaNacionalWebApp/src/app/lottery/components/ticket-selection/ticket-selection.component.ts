01671a44db71e28daa54a8d849ddcc41c3a57659
Jefferson Brioso <jabbsontouch@gmail.com>
2021-05-03 12:36:26 -0400

    Agregando componente ticke selection, fontawesome y creandsome y creando navbar

---
+++ loteriaNacionalWebApp/src/app/lottery/components/ticket-selection/ticket-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-selection',
+  templateUrl: './ticket-selection.component.html',
+  styleUrls: ['./ticket-selection.component.scss']
+})
+export class TicketSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
