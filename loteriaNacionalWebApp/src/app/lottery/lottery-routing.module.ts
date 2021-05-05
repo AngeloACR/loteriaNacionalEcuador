0dc2ef34617d5c04e5cdb0f17a52a963bee21b26
Jefferson Brioso <jabbsontouch@gmail.com>
2021-05-05 11:32:12 -0400

    Agregando seccion de seleccion, botones y menu latenu latral

--- loteriaNacionalWebApp/src/app/lottery/lottery-routing.module.ts
+++ loteriaNacionalWebApp/src/app/lottery/lottery-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoteriaComponent } from './components/loteria/loteria.component';
+import { LottoComponent } from './components/lotto/lotto.component';
+import { PozoMillonarioComponent } from './components/pozo-millonario/pozo-millonario.component';
 
 import { LotteryComponent } from './lottery.component';
 
-const routes: Routes = [{ path: '', component: LotteryComponent }];
+const routes: Routes = [
+  { 
+    path: '', component: LotteryComponent ,
+    children: [
+      { path: 'loteria', component: LoteriaComponent },
+      { path: 'lotto', component: LottoComponent },
+      { path: 'pozo-millonario', component: PozoMillonarioComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
