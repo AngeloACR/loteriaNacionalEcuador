17c811763926973433af97f0ca22e1de76070c6e
jabbcode <jabbsontouch@gmail.com>
2021-06-05 15:22:42 -0400

    agregando funcion para imagen de boletin

--- loteriaNacionalWebApp/src/app/inquiry/components/lotto-boletin/lotto-boletin.component.ts
+++ loteriaNacionalWebApp/src/app/inquiry/components/lotto-boletin/lotto-boletin.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { InquiryService } from "../../services/inquiry.service";
+
 @Component({
   selector: 'app-lotto-boletin',
   templateUrl: './lotto-boletin.component.html',
   styleUrls: ['./lotto-boletin.component.scss']
 })
 export class LottoBoletinComponent implements OnInit {
+  boletinImagen: any;
 
-  constructor() { }
+  constructor(private inquiryService: InquiryService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    /* console.log("En la consulta de loteria"); */
+    this.boletinImagen = await this.inquiryService.obtenerBoletin(2,0);
   }
-
 }
