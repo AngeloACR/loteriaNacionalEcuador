3e89ffa22b17a261450b746dfc1189fa1eb5cc03
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-06-06 03:05:36 -0400

    Modificando el puerto del api

--- loteriaNacionalServer/app.js
+++ loteriaNacionalServer/app.js
@@ -3,14 +3,14 @@ const webServer = require('./webServer');
 const userServer = require('./userServer');
 const config = require('./config/environment');
 
-const webPort = config.webPort;
+/* const webPort = config.webPort;
 const webFolder = './public/loteriaNacionalWebApp';
 const webPath = webFolder + '/index.html';
 const webApp = webServer.init(webFolder, webPath, webPort);
 
 webApp.listen(webPort, () => {
     console.log('Server running at: ' + webPort);
-});
+}); */
 
 const lotteryPort = config.lotteryPort;
 const lotteryApp = lotteryServer.init(lotteryPort);
@@ -18,10 +18,10 @@ const lotteryApp = lotteryServer.init(lotteryPort);
 lotteryApp.listen(lotteryPort, () => {
     console.log('Server running at: ' + lotteryPort);
 });
-
+/*
 const userPort = config.userPort;
 const userApp = userServer.init(userPort);
 
 userApp.listen(userPort, () => {
     console.log('Server running at: ' + userPort);
-});
+}); */
