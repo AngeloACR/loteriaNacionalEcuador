04a3b6b84a3d3effff185adc07593457ce3b49c2
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-06-08 23:40:12 -0400

    Generando build nuevo del front end

--- loteriaNacionalWebApp/src/app/inquiry/services/inquiry.service.ts
+++ loteriaNacionalWebApp/src/app/inquiry/services/inquiry.service.ts
@@ -2,12 +2,12 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 
 @Injectable({
-  providedIn: "root",
+  providedIn: "root"
 })
 export class InquiryService {
   today = new Date();
   localSource = "http://localhost:3400";
-  testSource = "https://api-loteria.gustavoliver.com";
+  testSource = "http://api-loteria.gustavoliver.com";
   productionSource = "";
 
   //mySource = this.localSource;
@@ -50,14 +50,13 @@ export class InquiryService {
   }
 
   obtenerBoletin(tipoLoteria, sorteo) {
-
     switch (tipoLoteria) {
       case 1:
-        return 'ruta de imagen de loteria nacional';
+        return "ruta de imagen de loteria nacional";
       case 2:
-        return 'ruta de imagen de lotto';
+        return "ruta de imagen de lotto";
       case 3:
-        return 'ruta de imagen de pozo millonario';
+        return "ruta de imagen de pozo millonario";
 
       default:
         break;
