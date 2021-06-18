7e867950afb38ad2fc2ea284e759378f33214464
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-06-18 02:19:43 -0400

    Compilando app de front, modificando config de nginx

--- loteriaNacionalServer/app.js
+++ loteriaNacionalServer/app.js
@@ -5,7 +5,7 @@ const userServer = require('./userServer');
 const config = require('./config/environment');
 
 const webPort = config.ventasPort;
-const webFolder = './public/loteriaNacionalWebApp';
+const webFolder = './public/loteriaNacionalWeb';
 const webPath = webFolder + '/index.html';
 const webApp = webServer.init(webFolder, webPath, webPort);
 
