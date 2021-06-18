9e0a5a38c6ae5b440e8e6f867a15eb75e4e4fe3a
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-06-18 10:30:38 -0400

    Otro cambio

--- loteriaNacionalServer/lottery/controller/results.js
+++ loteriaNacionalServer/lottery/controller/results.js
@@ -3,11 +3,12 @@ var xml2js = require('xml2js');
 var parser = xml2js.Parser();
 var fs = require('fs');
 
-module.exports.agregarResultados = async (req, res) => {
+module.exports.agregarResultados = async () => {
     try {
         let sorteo = req.body.sorteo;
         let tipoLoteria = req.body.tipoLoteria;
         let filePath = `uploads/resultados/PREM-${tipoLoteria}-${sorteo}.xml`
+
         fs.readFile(filePath, 'utf8', async function (err, xmlData) {
             if (err) throw err;
             let data = await parser.parseStringPromise(xmlData)
