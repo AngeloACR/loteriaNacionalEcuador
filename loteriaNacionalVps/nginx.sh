33984df5727d72b00115aaaab2a2722d0920505b
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-06-18 02:39:31 -0400

    Compilando app de front, modificando config de nginx

--- loteriaNacionalVps/nginx.sh
+++ loteriaNacionalVps/nginx.sh
@@ -3,7 +3,7 @@
 sudo mv ./proxy_params /etc/nginx/
 sudo rm /etc/nginx/nginx.conf
 sudo mv ./nginx.conf /etc/nginx/
-sudo mv ./nginx.conf /etc/nginx/default.d/ventas-api-prueba.conf
-sudo mv ./nginx.conf /etc/nginx/default.d/ventas-prueba.conf
-sudo mv ./nginx.conf /etc/nginx/default.d/contenidos-prueba.conf
+sudo mv ./ventas-api-prueba.conf /etc/nginx/default.d/
+sudo mv ./ventas-prueba.conf /etc/nginx/default.d/
+sudo mv ./contenidos-prueba.conf /etc/nginx/default.d/contenidos-prueba.conf
 sudo service nginx reload
