554d6cdc52671595af53a918a0b10f42076b4ce7
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-06-18 02:49:57 -0400

    Mejorando nginx

--- loteriaNacionalVps/nginx.sh
+++ loteriaNacionalVps/nginx.sh
@@ -6,4 +6,4 @@ sudo mv ./nginx.conf /etc/nginx/
 sudo mv ./ventas-api-prueba.conf /etc/nginx/conf.d/
 sudo mv ./ventas-prueba.conf /etc/nginx/conf.d/
 sudo mv ./contenidos-prueba.conf /etc/nginx/conf.d/
-sudo service nginx reload
+sudo service nginx start
