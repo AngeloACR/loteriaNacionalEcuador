af67726c486e850f56a67000ee5f86fb71cc65f1
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-07-01 22:09:51 -0400

    Cambiando algunos detalles de estilo

--- loteriaNacionalWebApp/src/app/landing/landing.component.ts
+++ loteriaNacionalWebApp/src/app/landing/landing.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class LandingComponent implements OnInit {
   link: string = "https://contenidos-prueba.loteria.com.ec/divi-slide";
+  linkNews: string = "https://contenidos-prueba.loteria.com.ec/newsletter";
   title: string = "sliderLoteria";
   constructor() {}
 
