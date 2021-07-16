942cf3398f4918f0eef088990ae414a4aad58118
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-07-15 22:12:45 -0400

    Corrigiendo algunos detalles de estilo

--- loteriaNacionalWebApp/src/app/inquiry/services/inquiry.service.ts
+++ loteriaNacionalWebApp/src/app/inquiry/services/inquiry.service.ts
@@ -11,8 +11,8 @@ export class InquiryService {
   productionSource = "https://ventas-api.loteria.com.ec";
 
   //mySource = this.localSource;
-  //mySource = this.testSource;
-  mySource = this.productionSource;
+  mySource = this.testSource;
+  //mySource = this.productionSource;
 
   constructor(private http: HttpClient) {}
 
