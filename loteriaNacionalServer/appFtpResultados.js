f97da64282f389f4e9bde233f484575c41fb540e
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-07-21 04:37:17 -0400

    Corrigiendo error de produccion

--- loteriaNacionalServer/appFtpResultados.js
+++ loteriaNacionalServer/appFtpResultados.js
@@ -2,12 +2,12 @@
 const ftpResultadosServer = require('./ftpResultadosServer');
 const config = require('./config/environment');
 
-const ftpResultadosPort = config.ftpResultadosPort;
-const ftpResultadosApp = ftpResultadosServer.init(ftpHost, ftpResultadosPort);
-
 //const ftpHost = config.ftpHostLocal;
 //const ftpHost = config.ftpHostTest;
 const ftpHost = config.ftpHostProd;
+const ftpResultadosPort = config.ftpResultadosPort;
+const ftpResultadosApp = ftpResultadosServer.init(ftpHost, ftpResultadosPort);
+
 
 ftpResultadosApp.listen()
     .then(() => {
