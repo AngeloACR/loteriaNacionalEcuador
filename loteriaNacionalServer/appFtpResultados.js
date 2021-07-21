f8045e8d8fa888fba042e50732022ba951a72c80
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-07-21 16:35:29 -0400

    Probando ftp

--- loteriaNacionalServer/appFtpResultados.js
+++ loteriaNacionalServer/appFtpResultados.js
@@ -3,8 +3,8 @@ const ftpResultadosServer = require('./ftpResultadosServer');
 const config = require('./config/environment');
 
 //const ftpHost = config.ftpHostLocal;
-//const ftpHost = config.ftpHostTest;
-const ftpHost = config.ftpHostProd;
+const ftpHost = config.ftpHostTest;
+//const ftpHost = config.ftpHostProd;
 const ftpResultadosPort = config.ftpResultadosPort;
 const ftpResultadosApp = ftpResultadosServer.init(ftpHost, ftpResultadosPort);
 
