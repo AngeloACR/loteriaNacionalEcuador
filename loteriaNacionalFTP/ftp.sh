04fa248f1c57427a1f01eeb9ae08ef8e2c85eafa
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-07-21 01:09:04 -0400

    Probando FTP

--- loteriaNacionalFTP/ftp.sh
+++ loteriaNacionalFTP/ftp.sh
@@ -5,7 +5,7 @@ tail -F /var/log/xferlog | while read line; do
     IFS=" " read -a myarray <<< $line
     filename=${myarray[8]}
     if [[ $filename == *"BOLPRE"* ]]; then
-        echo "It's Resultados!"
+        python3 ./resultadosFTP.py $filename
     fi    
     if [[ $filename == *"PREM"* ]]; then
         python3 ./premiosFTP.py $filename
