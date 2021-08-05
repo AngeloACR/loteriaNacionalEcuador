6af32724b923be52663bedaf17a32d3f9f4c3ba6
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-08-05 13:56:42 -0400

    Integrando los sorteos y boletos disponibles

--- loteriaNacionalServer/appFtpBoletos.js
+++ loteriaNacionalServer/appFtpBoletos.js
@@ -2,8 +2,8 @@ const ftpBoletosServer = require('./ftpBoletosServer');
 const config = require('./config/environment');
 
 //const ftpHost = config.ftpHostLocal;
-//const ftpHost = config.ftpHostTest;
-const ftpHost = config.ftpHostProd;
+const ftpHost = config.ftpHostTest;
+//const ftpHost = config.ftpHostProd;
 const ftpBoletosPort = config.ftpBoletosPort;
 const ftpBoletosApp = ftpBoletosServer.init(ftpHost, ftpBoletosPort);
 
