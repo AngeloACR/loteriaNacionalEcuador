c552821b92bc9c2d8a5ea38367f645dba80ea3c6
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-08-10 03:41:32 -0400

    Integrando esquema de autorizacion de exxalogic

--- loteriaNacionalWebApp/src/app/lottery/components/seleccion-tipo/seleccion-tipo.component.ts
+++ loteriaNacionalWebApp/src/app/lottery/components/seleccion-tipo/seleccion-tipo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: 'app-seleccion-tipo',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seleccion-tipo.component.scss']
 })
 export class SeleccionTipoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  linkLotto: string;
+  linkLoteriaNacional: string;
+  linkPozoMillonario: string;
+  token: string;
+  usuario: string;
+  constructor(private actRoute: ActivatedRoute) {
+    this.actRoute.params.subscribe(params => {
+      this.token = params["token"];
+      this.usuario = params["username"];
+    });
   }
 
+  ngOnInit() {
+    this.linkLoteriaNacional = `/compra_boletos/${this.token}/${this.usuario}/loteria`;
+    this.linkLotto = `/compra_boletos/${this.token}/${this.usuario}/lotto`;
+    this.linkPozoMillonario = `/compra_boletos/${this.token}/${this.usuario}/pozo-millonario`;
+  }
 }
