8a921282a69bd4490f86b330d072b5ae5ab9a0a1
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-09-07 21:26:44 -0400

    Reparando la consulta

--- loteriaNacionalServer/appFtpBoletines.js
+++ loteriaNacionalServer/appFtpBoletines.js
@@ -2,8 +2,8 @@ const ftpBoletinesServer = require('./ftpBoletinesServer');
 const config = require('./config/environment');
 
 //const ftpHost = config.ftpHostLocal;
-const ftpHost = config.ftpHostTest;
-//const ftpHost = config.ftpHostProd;
+//const ftpHost = config.ftpHostTest;
+const ftpHost = config.ftpHostProd;
 const ftpBoletinesPort = config.ftpBoletinesPort;
 const ftpBoletinesApp = ftpBoletinesServer.init(ftpHost, ftpBoletinesPort);
 
