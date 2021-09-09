987ce06907ad0ce3f0aef97d7d0d842360d86d6f
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-09-09 01:24:33 -0400

    Reparando consulta

--- loteriaNacionalServer/results/controller/resultados.js
+++ loteriaNacionalServer/results/controller/resultados.js
@@ -214,6 +214,7 @@ const resultadosController = {
                 status: false,
                 msg: error.toString().replace("Error: ", "")
             }
+            console.log(error);
             return response
         }
     },
