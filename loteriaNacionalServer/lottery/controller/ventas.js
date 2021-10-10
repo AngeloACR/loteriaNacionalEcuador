f0f081c9b43f9b6c820097af3e49ddbfafd91b06
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-10-10 19:13:09 -0400

    Integrando boletos

--- loteriaNacionalServer/lottery/controller/ventas.js
+++ loteriaNacionalServer/lottery/controller/ventas.js
@@ -13,8 +13,8 @@ const ganadoresController = require("./ganadores");
 /*************************** CONSULTA DE RESULTADOS************************/
 
 //let sourceBoletos = config.sourceBoletosLocal;
-//let sourceBoletos = config.sourceBoletosTest;
-let sourceBoletos = config.sourceBoletosProd;
+let sourceBoletos = config.sourceBoletosTest;
+//let sourceBoletos = config.sourceBoletosProd;
 
 function getCurrentTimeStamp() {
   let date = new Date(Date.now()).toLocaleString("es-EC", {
