2d86d5b1a75544ad6f5a4cfe808864caad1b2a93
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-10-18 14:51:07 -0400

    Corrigiendo loader

--- loteriaNacionalWebApp/src/app/shared/components/loader/loader.component.ts
+++ loteriaNacionalWebApp/src/app/shared/components/loader/loader.component.ts
@@ -8,43 +8,25 @@ import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 export class LoaderComponent implements OnInit {
   @Input() message: string;
   @Input() loader: string;
-  loaderPath: string;
+  loaderPath: string = "assets/loader.gif";
+  logoPath: string;
   constructor() {}
 
-  animator: {};
-  animatorStatus: boolean = true;
   interval: any;
   ngOnInit() {
     switch (this.loader) {
       case "lotto":
-        this.loaderPath = "assets/img/lotto-sombra-2.png";
-        //this.loaderPath = "assets/lottoLoader.gif";
+        this.logoPath = "assets/img/lotto-sombra-2.png";
+        //this.logoPath = "assets/lottoLoader.gif";
         break;
       case "pozo":
-        this.loaderPath = "assets/img/pozo-sombra.png";
-        //this.loaderPath = "assets/pozoLoader.gif";
+        this.logoPath = "assets/img/pozo-sombra.png";
+        //this.logoPath = "assets/pozoLoader.gif";
         break;
       default:
-        this.loaderPath = "assets/img/loteria-sombra.png";
-        //this.loaderPath = "assets/loteriaLoader.gif";
+        this.logoPath = "assets/img/loteria-sombra.png";
+        //this.logoPath = "assets/loteriaLoader.gif";
         break;
     }
-
-    setTimeout(() => {
-      this.animator = {
-        logoAnimado: this.animatorStatus,
-      };
-    }, 100);
-
-    this.interval = setInterval(() => {
-      this.animatorStatus = !this.animatorStatus;
-      this.animator = {
-        logoAnimado: this.animatorStatus,
-      };
-    }, 1000);
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.interval);
   }
 }
