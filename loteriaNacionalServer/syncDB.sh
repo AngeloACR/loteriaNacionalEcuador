09da12c90dab171b2e34cfed02870771c86258a2
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-11-08 03:45:52 -0400

    Sincronizando DB

--- loteriaNacionalServer/syncDB.sh
+++ loteriaNacionalServer/syncDB.sh
@@ -7,7 +7,7 @@ mongoexport --collection=premios --db=loteriaDB --out=premios-${now}.json
 mongoexport --collection=resultadoloterias --db=loteriaDB --out=resultadoloterias-${now}.json
 mongoexport --collection=resultadolottos --db=loteriaDB --out=resultadolottos-${now}.json
 mongoexport --collection=resultadopozos --db=loteriaDB --out=resultadopozos-${now}.json
+mongoexport --collection=ventas --db=loteriaDB --out=ventas-${now}.json
 mongoexport --collection=resultados --db=loteriaDB --out=resultados-${now}.json
 mongoexport --collection=sorteos --db=loteriaDB --out=sorteos-${now}.json
 mongoexport --collection=ultimoresultados --db=loteriaDB --out=ultimoresultados-${now}.json
-mongoexport --collection=ventas --db=loteriaDB --out=ventas-${now}.json
