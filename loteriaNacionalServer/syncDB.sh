1d40b01ce48e0bf27632040922306a50ecf701f7
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-11-11 05:23:15 -0400

    Corrigiendo tema de backup

--- loteriaNacionalServer/syncDB.sh
+++ loteriaNacionalServer/syncDB.sh
@@ -2,12 +2,12 @@
 
 now=$(date)
 
-mongoexport --collection=ganadoreswebs --db=loteriaDB --out=/home/loterianacional/respladoDB/"ganadoresweb-${now}".json
-mongoexport --collection=premios --db=loteriaDB --out=/home/loterianacional/respladoDB/"premios-${now}".json
-mongoexport --collection=resultadoloterias --db=loteriaDB --out=/home/loterianacional/respladoDB/"resultadoloterias-${now}".json
-mongoexport --collection=resultadolottos --db=loteriaDB --out=/home/loterianacional/respladoDB/"resultadolottos-${now}".json
-mongoexport --collection=resultadopozos --db=loteriaDB --out=/home/loterianacional/respladoDB/"resultadopozos-${now}".json
-mongoexport --collection=ventas --db=loteriaDB --out=/home/loterianacional/respladoDB/"ventas-${now}".json
-mongoexport --collection=resultados --db=loteriaDB --out=/home/loterianacional/respladoDB/"resultados-${now}".json
-mongoexport --collection=sorteos --db=loteriaDB --out=/home/loterianacional/respladoDB/"sorteos-${now}".json
-mongoexport --collection=ultimoresultados --db=loteriaDB --out=/home/loterianacional/respladoDB/"ultimoresultados-${now}".json
+mongoexport --collection=ganadoreswebs --db=loteriaDB --out=/home/loterianacional/respaldoDB/"ganadoresweb-${now}".json
+mongoexport --collection=premios --db=loteriaDB --out=/home/loterianacional/respaldoDB/"premios-${now}".json
+mongoexport --collection=resultadoloterias --db=loteriaDB --out=/home/loterianacional/respaldoDB/"resultadoloterias-${now}".json
+mongoexport --collection=resultadolottos --db=loteriaDB --out=/home/loterianacional/respaldoDB/"resultadolottos-${now}".json
+mongoexport --collection=resultadopozos --db=loteriaDB --out=/home/loterianacional/respaldoDB/"resultadopozos-${now}".json
+mongoexport --collection=ventas --db=loteriaDB --out=/home/loterianacional/respaldoDB/"ventas-${now}".json
+mongoexport --collection=resultados --db=loteriaDB --out=/home/loterianacional/respaldoDB/"resultados-${now}".json
+mongoexport --collection=sorteos --db=loteriaDB --out=/home/loterianacional/respaldoDB/"sorteos-${now}".json
+mongoexport --collection=ultimoresultados --db=loteriaDB --out=/home/loterianacional/respaldoDB/"ultimoresultados-${now}".json
