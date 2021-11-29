d67598fb705c7ddbcd90e654c2f825040fcbd62a
Angelo Crincoli <angelocrincoli91@gmail.com>
2021-11-29 04:59:16 -0400

    Corrigiendo ultimos resultados

--- loteriaNacionalServer/results/routes/results.js
+++ loteriaNacionalServer/results/routes/results.js
@@ -7,7 +7,7 @@ resultsRouter.get('/ultimoResultadoPozo', ResultsController.ultimoResultadoPozo)
 resultsRouter.get('/agregarUltimoResultadoPozo', ResultsController.agregarUltimoResultadoPozo);
 resultsRouter.get('/limpiarDB', ResultsController.limpiarDBHttp);
 resultsRouter.get('/parseResultados', ResultsController.parseResultadosHttp);
-resultsRouter.get('/actualizarUltimosResultados', ResultsController.actualizarUltimosResultados);
+resultsRouter.get('/actualizarUltimosResultados', ResultsController.actualizarUltimosResultadosHttp);
 
 
 module.exports = resultsRouter;
