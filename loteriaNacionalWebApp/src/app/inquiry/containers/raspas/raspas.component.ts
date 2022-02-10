a4c1905c9e0e1c7dedbde4b0b61962fbf6f4af2c
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-02-09 23:44:40 -0400

    Preparando deploy a test

--- loteriaNacionalWebApp/src/app/inquiry/containers/raspas/raspas.component.ts
+++ loteriaNacionalWebApp/src/app/inquiry/containers/raspas/raspas.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-raspas',
-  templateUrl: './raspas.component.html',
-  styleUrls: ['./raspas.component.scss']
+  selector: "app-raspas",
+  templateUrl: "./raspas.component.html",
+  styleUrls: ["./raspas.component.scss"],
 })
 export class RaspasComponent implements OnInit {
+  showResultados: boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
+  toggleResultados(event) {}
 }
