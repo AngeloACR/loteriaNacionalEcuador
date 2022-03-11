7166a2b6593bd55d40d9cd028e5bea26316be6dc
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-03-11 09:25:32 -0400

    Corrigiendo estilo de la millonaria

--- loteriaNacionalWebApp/src/app/lottery/components/ticket-scroller/ticket-scroller.component.ts
+++ loteriaNacionalWebApp/src/app/lottery/components/ticket-scroller/ticket-scroller.component.ts
@@ -65,7 +65,7 @@ export class TicketScrollerComponent implements OnInit {
 
       case "millonaria":
         this.isLaMillonaria = true;
-        this.logoPath = "assets/img/loteria-carrito.svg";
+        this.logoPath = "assets/img/lotto-carrito.svg";
         break;
     }
     this.getTotal();
