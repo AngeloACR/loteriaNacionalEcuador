2ea7c26f86608eb7989f6af58e1208d03a095980
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-03-18 22:10:39 -0400

    Agregando heapdump

--- loteriaNacionalServer/appLotteryApi.js
+++ loteriaNacionalServer/appLotteryApi.js
@@ -1,8 +1,8 @@
-const db = require('./database');
-const lotteryServer = require('./lotteryServer');
-const webServer = require('./webServer');
-const config = require('./config/environment');
-const cronjobs = require('./cronjobs');
+const db = require("./database");
+const lotteryServer = require("./lotteryServer");
+const webServer = require("./webServer");
+const config = require("./config/environment");
+const cronjobs = require("./cronjobs");
 
 // https://www.nginx.com/blog/nginx-caching-guide/
 const connection = db.initConnect();
@@ -11,10 +11,13 @@ const lotteryPort = config.lotteryPort;
 const lotteryApp = lotteryServer.init(lotteryPort);
 
 lotteryApp.listen(lotteryPort, () => {
-    console.log('Server running at: ' + lotteryPort);
+  console.log("Server running at: " + lotteryPort);
+  console.log(
+    `Heapdump enabled. Run "kill -USR2 ${process.pid}" or send a request to "/heapdump" to generate a heapdump.`
+  );
 });
 
 let jobs = cronjobs.setCronJobs();
-jobs.forEach(job => {
-    job.start();
+jobs.forEach((job) => {
+  job.start();
 });
