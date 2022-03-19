5ec5c9da1bc46fd9859f85fce4ecaaefe4b610c5
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-03-18 22:29:29 -0400

    Agregando heapdump

--- loteriaNacionalServer/heapdump/routes/main.js
+++ loteriaNacionalServer/heapdump/routes/main.js
@@ -2,6 +2,6 @@ const express = require("express");
 const lotteryRouter = express.Router();
 const HeapdumpController = require("../controller/main");
 
-lotteryRouter.post("/", HeapdumpController.print);
+lotteryRouter.get("/", HeapdumpController.print);
 
 module.exports = lotteryRouter;
