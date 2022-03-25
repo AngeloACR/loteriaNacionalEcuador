0e7b343af80e29eac99906179144135c726530a9
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-03-25 09:04:50 -0400

    Reparando orden de sorteos

--- loteriaNacionalServer/lotteryServer.js
+++ loteriaNacionalServer/lotteryServer.js
@@ -41,7 +41,7 @@ module.exports.init = function (port) {
 
   app.use('/boletines', express.static('uploads/boletines'));
   app.use('/boletos', express.static('uploads/boletos'));
-  app.use('/logs', express.static('logs'));
+  //app.use('/logs', express.static('logs'));
 
   //Adding routes
 
