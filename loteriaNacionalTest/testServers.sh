72f10cc42a9c2d49003477cddaf841c3eda54d97
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-05-22 04:09:10 -0400

    Implementando benchmark test

--- loteriaNacionalTest/testServers.sh
+++ loteriaNacionalTest/testServers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ab -p loteriaData.txt -n 300 -c 10 http://localhost:5500/ganador > loteriaTestResults.txt &
-ab -p lottoData.txt -n 300 -c 10 http://localhost:6000/ganador > lottoTestResults.txt &
-ab -p pozoData.txt -n 300 -c 10 http://localhost:6500/ganador > pozoTestResults.txt &
-ab -p millonariaData.txt -n 300 -c 10 http://localhost:7000/ganador > millonariaTestResults.txt &
+ab -p loteriaData.txt -T application/json -n 300 -c 10 http://localhost:5500/ganador > loteriaTestResults.txt &
+ab -p lottoData.txt -T application/json -n 300 -c 10 http://localhost:6000/ganador > lottoTestResults.txt &
+ab -p pozoData.txt -T application/json -n 300 -c 10 http://localhost:6500/ganador > pozoTestResults.txt &
+ab -p millonariaData.txt -T application/json -n 300 -c 10 http://localhost:7000/ganador > millonariaTestResults.txt &
