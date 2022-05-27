7e54ea9d2c1746e69cafbf112aebdce2610eec97
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-05-26 20:15:44 -0400

    Configurando a localhost

--- loteriaNacionalServer2/runServer.sh
+++ loteriaNacionalServer2/runServer.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# cd auth
-# pm2 start authApp.js --name authApp
+ cd auth
+ pm2 start authApp.js --name authApp
 # cd ../ftpBoletines
 # pm2 start ftpBoletinesApp.js --name ftpBoletinesApp
 # cd ../ftpBoletos
 # pm2 start ftpBoletosApp.js --name ftpBoletosApp
-# cd ../ganadores
-# pm2 start ganadoresApp.js --name ganadoresApp
+ cd ../ganadores
+ pm2 start ganadoresApp.js --name ganadoresApp
 # cd ../helper
 # pm2 start helperApp.js --name helperApp
-# cd ../reservas
-# pm2 start reservasApp.js --name reservasApp
-cd  ./sorteosLaMillonaria
+ cd ../reservas
+ pm2 start reservasApp.js --name reservasApp
+cd  ../sorteosLaMillonaria
 pm2 start laMillonariaApp.js --name laMillonariaApp
 cd ../sorteosLoteriaNacional
 pm2 start loteriaNacionalApp.js --name loteriaNacionalApp
@@ -22,9 +22,9 @@ cd ../sorteosPozoMillonario
 pm2 start pozoMillonarioApp.js --name pozoMillonarioApp
 # cd ../uploads
 # pm2 start uploadsApp.js --name uploadsApp
-# cd ../ventas
-# pm2 start ventasApp.js --name ventasApp
-# cd ../wallet
-# pm2 start walletApp.js --name walletApp
+ cd ../ventas
+ pm2 start ventasApp.js --name ventasApp
+ cd ../wallet
+ pm2 start walletApp.js --name walletApp
 # cd ../web
 # pm2 start webApp.js --name webApp
