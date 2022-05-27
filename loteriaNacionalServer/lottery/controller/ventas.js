c985b675ff04b6133b6590fef4bbe48db2bb8483
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-05-26 22:33:43 -0400

    Adecuando lotto para instantanea

--- loteriaNacionalServer/lottery/controller/ventas.js
+++ loteriaNacionalServer/lottery/controller/ventas.js
@@ -703,40 +703,14 @@ const ventasController = {
           for (let i = 0; i < instantanea.premios.length; i++) {
             const premio = instantanea.premios[i];
 
-            let sorteos;
             let nombreLoteria;
             let tipoLoteria = parseInt(instantanea.sorteo.JId);
-            switch (tipoLoteria) {
-              case 1:
-                nombreLoteria = "Loteria Nacional";
-                sorteos = loteriaSorteos;
-                break;
-              case 2:
-                nombreLoteria = "Lotto";
-                sorteos = lottoSorteos;
-                break;
 
-              case 5:
-                nombreLoteria = "Pozo Millonario";
-                sorteos = pozoSorteos;
-                break;
-              case 14:
-                nombreLoteria = "La Millonaria";
-                sorteos = millonariaSorteos;
-                break;
-            }
-            let drawDateAux = sorteos
-              .find((sorteo) => sorteo.sorteo == instantanea.sorteo.Sort)
-              .fecha.split(" ")[0]
-              .split("/");
-            let drawDate = `${drawDateAux[2]}-${drawDateAux[1]}-${drawDateAux[0]}`;
             let prizeDetail = {
               lotteryType: tipoLoteria,
-              lotteryName: nombreLoteria,
               drawNumber: parseInt(instantanea.sorteo.Sort),
-              drawDate,
               combinationC1: premio.Num,
-              fractions: premio.Fra,
+              lotteryName: nombreLoteria,
               prize: parseFloat(premio.Val).toFixed(2),
               prizeWithDiscount: parseFloat(premio.ConDesc).toFixed(2),
               prizeDescription: premio.Prem.normalize("NFD").replace(
@@ -744,7 +718,63 @@ const ventasController = {
                 ""
               ),
             };
-            if (premio.Num2) prizeDetail["combinationC2"] = premio.Num2;
+
+            let drawDateAux;
+            switch (tipoLoteria) {
+              case 1:
+                prizeDetail["lotteryName"] = "Loteria Nacional";
+                drawDateAux = loteriaSorteos
+                  .find((sorteo) => sorteo.sorteo == instantanea.sorteo.Sort)
+                  .fecha.split(" ")[0]
+                  .split("/");
+                prizeDetail[
+                  "drawDate"
+                ] = `${drawDateAux[2]}-${drawDateAux[1]}-${drawDateAux[0]}`;
+                prizeDetail["fractions"] = premio.Fra;
+
+                break;
+              case 2:
+                prizeDetail["lotteryName"] = "Lotto";
+                drawDateAux = lottoSorteos
+                  .find((sorteo) => sorteo.sorteo == instantanea.sorteo.Sort)
+                  .fecha.split(" ")[0]
+                  .split("/");
+                prizeDetail[
+                  "drawDate"
+                ] = `${drawDateAux[2]}-${drawDateAux[1]}-${drawDateAux[0]}`;
+                let item = reservationDetails.find(
+                  (element) =>
+                    prizeDetail.combinationC1 == element.combinationC1
+                );
+                prizeDetail["combinationC2"] = item.combinationC2;
+                prizeDetail["combinationC3"] = item.combinationC3;
+                prizeDetail["combinationC4"] = item.combinationC4;
+                prizeDetail["combinationC5"] = item.combinationC5;
+                break;
+
+              case 5:
+                prizeDetail["lotteryName"] = "Pozo Millonario";
+                drawDateAux = pozoSorteos
+                  .find((sorteo) => sorteo.sorteo == instantanea.sorteo.Sort)
+                  .fecha.split(" ")[0]
+                  .split("/");
+                prizeDetail[
+                  "drawDate"
+                ] = `${drawDateAux[2]}-${drawDateAux[1]}-${drawDateAux[0]}`;
+                break;
+              case 14:
+                prizeDetail["lotteryName"] = "La Millonaria";
+                drawDateAux = millonariaSorteos
+                  .find((sorteo) => sorteo.sorteo == instantanea.sorteo.Sort)
+                  .fecha.split(" ")[0]
+                  .split("/");
+                prizeDetail[
+                  "drawDate"
+                ] = `${drawDateAux[2]}-${drawDateAux[1]}-${drawDateAux[0]}`;
+                prizeDetail["fractions"] = premio.Fra;
+                prizeDetail["combinationC2"] = premio.Num2;
+                break;
+            }
             let ganador = {
               personaId: personaId,
               tipoLoteria: tipoLoteria,
@@ -758,13 +788,13 @@ const ventasController = {
               ventaId: loteriaVentaResponse.ticketId,
               acreditado: true,
             };
-            await ganadoresController.crearGanador(ganador);
+            await Ganadores.crearGanador(ganador);
             logData = {
               data: ganador,
               response: loteriaVentaResponse,
-              function: "ganadoresController.crearGanador",
+              function: "Ganadores.crearGanador",
             };
-            apiVentasLogger.info("comprarBoletos.api", logData);
+            ventasLogger.info("comprarBoletos.api", logData);
             prizeDetails.push(prizeDetail);
           }
         }
@@ -1220,7 +1250,7 @@ const ventasController = {
       };
       res.status(400).json(response);
     }
-  },/* 
+  } /* 
   resolverVentasDeDescuadre: async (req, res) => {
 
     let data = req.body.data;
@@ -1232,9 +1262,8 @@ const ventasController = {
       response.push(venta);
     }
     res.status(200).json(response);
-  }, */
+  }, */,
   resolverVentasDeDescuadre: async (req, res) => {
-
     let data = req.body.data;
     let response = [];
     for (let i = 0; i < data.length; i++) {
@@ -1310,7 +1339,6 @@ const ventasController = {
           exaVentaId
         );
 
-  
       let instantaneaResponse = {
         status: instantaneaStatus,
         data: instantaneaData,
