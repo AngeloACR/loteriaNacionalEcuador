6d77723bc4a14481cfb3c4f51daa75d4e6e121d4
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-06-15 04:39:39 -0400

    Corrigiendo fallo con cedulas y nombres

--- loteriaNacionalServer2/codigosPromocionales/routes/main.js
+++ loteriaNacionalServer2/codigosPromocionales/routes/main.js
@@ -4,6 +4,7 @@ const mainController = require('../controller/main');
 
 lotteryRouter.get('/', mainController.populate);
 lotteryRouter.get('/getCodeCsv', mainController.getCodeCsvHttp);
+lotteryRouter.get('/getText', mainController.getText);
 lotteryRouter.post('/setCode', mainController.setCodeHttp);
 lotteryRouter.post('/getCodes', mainController.getCodes);
 
