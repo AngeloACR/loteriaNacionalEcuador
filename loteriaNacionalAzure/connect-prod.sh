7fa792421d8a8d37b4a6093b318f8a08b6cf983e
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-06-29 14:10:43 -0400

    Corrigiendo detalle de redireccion a bet593.ec en landing

---
+++ loteriaNacionalAzure/connect-prod.sh
@@ -0,0 +1,25 @@
+installDir="/etc/"
+virtualNetworkName="<Vnet-ECU-Prod01>"
+
+vpnServer=$(xmllint --xpath "string(/VpnProfile/VpnServer)" Generic/VpnSettings.xml)
+vpnType=$(xmllint --xpath "string(/VpnProfile/VpnType)" Generic/VpnSettings.xml | tr '[:upper:]' '[:lower:]')
+routes=$(xmllint --xpath "string(/VpnProfile/Routes)" Generic/VpnSettings.xml)
+
+sudo cp "${installDir}ipsec.conf" "${installDir}ipsec.conf.backup"
+sudo cp "Generic/VpnServerRoot.cer" "${installDir}ipsec.d/cacerts"
+
+echo -e "\nconn $virtualNetworkName" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+echo -e "\tkeyexchange=$vpnType" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+echo -e "\ttype=tunnel" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+echo -e "\tleftfirewall=yes" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+echo -e "\tleft=%any" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+echo -e "\tleftauth=eap-tls" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+echo -e "\tleftid=%client" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+echo -e "\tright=$vpnServer" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+echo -e "\trightid=%$vpnServer" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+echo -e "\trightsubnet=$routes" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+echo -e "\tleftsourceip=%config" | sudo tee -a "${installDir}ipsec.conf" > /dev/null 
+echo -e "\tauto=add" | sudo tee -a "${installDir}ipsec.conf" > /dev/null
+
+sudo ipsec restart
+sudo ipsec up $virtualNetworkName
