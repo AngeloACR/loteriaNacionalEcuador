d0e93bfe4ad9a170468511fc3127c6f2b8b32572
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-09-06 10:19:42 -0400

    Removiendo archivos innecesarios

--- loteriaNacionalTest/testServers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ab -p loteriaData.txt -T application/json -n 400 -c 5 http://localhost:5500/ganador > loteriaTestResults.txt
-ab -p lottoData.txt -T application/json -n 400 -c 5 http://localhost:6000/ganador > lottoTestResults.txt
-ab -p pozoData.txt -T application/json -n 400 -c 5 http://localhost:6500/ganador > pozoTestResults.txt
-ab -p millonariaData.txt -T application/json -n 400 -c 5 http://localhost:7000/ganador > millonariaTestResults.txt
