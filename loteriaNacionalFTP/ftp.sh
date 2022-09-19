1ef18598001ea0b2c71bb4f90aa0c9821fc94068
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-09-19 01:19:00 -0400

    Preparando deploy de prueba de ftp

--- loteriaNacionalFTP/ftp.sh
+++ loteriaNacionalFTP/ftp.sh
@@ -5,13 +5,13 @@ tail -F /home/acri/ftpLogs/xferlog | while read line; do
     IFS=" " read -a myarray <<< $line
     filename=${myarray[8]}
     if [[ $filename == *"BOLPRE"* ]]; then
-        python3 ./resultadosFTP.py $filename
+        python3.6 ./resultadosFTP.py $filename
     fi    
     if [[ $filename == *"PREM"* ]]; then
-        python3 ./premiosFTP.py $filename
+        python3.6 ./premiosFTP.py $filename
     fi        
     if [[ $filename == *"GANWEB"* ]]; then
-        python3 ./ganwebFTP.py $filename
+        python3.6 ./ganwebFTP.py $filename
     fi    
   fi
 done
