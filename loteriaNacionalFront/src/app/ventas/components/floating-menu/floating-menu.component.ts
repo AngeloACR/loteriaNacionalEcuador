4ff64b3b318790c12e09426ffdaeb97d23738948
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-10-05 11:05:16 -0400

    Trabajando en boton flotante

--- loteriaNacionalFront/src/app/ventas/components/floating-menu/floating-menu.component.ts
+++ loteriaNacionalFront/src/app/ventas/components/floating-menu/floating-menu.component.ts
@@ -18,7 +18,7 @@ export class FloatingMenuComponent implements OnInit {
   isToggled: boolean = false;
   @ViewChild('float') floatingBox: any;
 
-  @HostListener('document:scroll', ['$event']) 
+  @HostListener('scroll', ['$event']) 
   handleScroll(e: any) {
     console.log(e.scrollTop);
     let position = window.innerWidth -80 + e.scrollTop;
