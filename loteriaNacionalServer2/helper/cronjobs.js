1fec0892e061b4024790dc8639e3ac54f885d90d
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-10-13 22:34:52 -0400

    Preparando prueba de cronjob de alarma

--- loteriaNacionalServer2/helper/cronjobs.js
+++ loteriaNacionalServer2/helper/cronjobs.js
@@ -1,11 +1,12 @@
 var CronJob = require('cron').CronJob;
+const Controller = require("./controller/main");
 
 module.exports.setCronJobs = function () {
     let jobs = [];
     //let jobTime = '00 * * * * *'
-    //let jobTimeA = '00 00 00/3 * * *'
-    //var jobA = new CronJob(jobTimeA, Resultados.agregarSorteos, null, true, 'America/Guayaquil');
-    //jobs.push(jobA)
+  let jobTimeA = '00 00 11 * * *'
+  var jobA = new CronJob(jobTimeA, Controller.alertaArchivos, null, true, 'America/Guayaquil');
+  jobs.push(jobA)
     //let jobTimeB = '00 15 00/1 * * *'
     //let jobTimeC = '00 30 00/1 * * *'
     //let jobTimeD = '00 45 00/1 * * *'
