280ebdbf5e0d90d3a6081bc56e81c9219b2a0a60
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-11-07 15:41:44 -0400

    Preparando nueva tanda de codigos promocionales

--- loteriaNacionalServer2/codigosPromocionales/test.js
+++ loteriaNacionalServer2/codigosPromocionales/test.js
@@ -1,7 +1,10 @@
 const fs = require("fs").promises;
 const config = require("../environments/test");
-const CodigoPromocional = require("./models/main")(async () => {
+const CodigoPromocional = require("./models/main");
+(async () => {
   try {
+    await CodigoPromocional.populate();
+    console.log("Done");
   } catch (error) {
     console.log(error.message);
   }
