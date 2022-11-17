eeb5701d8f615c138271424701583bd81eec5c19
Angelo Crincoli <angelocrincoli91@gmail.com>
2022-11-17 00:11:26 -0400

    Corrigiendo api de ftp

--- loteriaNacionalFTP/api.py
+++ loteriaNacionalFTP/api.py
@@ -1,4 +1,5 @@
 import flask
+import subprocess
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -6,14 +7,17 @@ app.config["DEBUG"] = True
 
 @app.route('/ganadores/<archivo>', methods=['GET'])
 def ganadores(archivo):
+    subprocess.call("python3.6 ganwebFTP.py "+archivo, shell=True)
     return archivo
 
 @app.route('/premios/<archivo>', methods=['GET'])
 def premios(archivo):
+    subprocess.call("python3.6 premiosFTP.py "+archivo, shell=True)
     return archivo
 
 @app.route('/resultados/<archivo>', methods=['GET'])
 def resultados(archivo):
+    subprocess.call("python3.6 resultadosFTP.py "+archivo, shell=True)
     return archivo
 
 app.run()
