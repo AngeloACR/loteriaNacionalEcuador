ccc0f7e377d103c699e97fef2443dd6564ab774b
Angelo Crincoli <angelocrincoli91@gmail.com>
2023-02-21 19:24:07 -0400

    Probando flotante

--- loteriaNacionalFront/src/app/shared/positioning.directive.ts
+++ loteriaNacionalFront/src/app/shared/positioning.directive.ts
@@ -15,8 +15,8 @@ export class PositioningDirective implements OnInit {
     let data = e.data.top ? e.data.top + e.data.screen : 120;
     let totalOffset = this.offset + this.el.nativeElement.offsetHeight / 2;
     let position = Math.min(
-      data - totalOffset,
-      e.target.innerHeight - totalOffset
+      Math.abs(data - totalOffset),
+      Math.abs(e.target.innerHeight - totalOffset)
     );
     position = position > 0 ? position : 0;
     this.renderer.setStyle(this.el.nativeElement, 'top', `${position}px`);
--- loteriaNacionalServer2/web/public/main.js
+++ loteriaNacionalServer2/web/public/main.js
@@ -6707,7 +6707,7 @@ class PositioningDirective {
     handleScroll(e) {
         let data = e.data.top ? e.data.top + e.data.screen : 120;
         let totalOffset = this.offset + this.el.nativeElement.offsetHeight / 2;
-        let position = Math.min(data - totalOffset, e.target.innerHeight - totalOffset);
+        let position = Math.min(Math.abs(data - totalOffset), Math.abs(e.target.innerHeight - totalOffset));
         position = position > 0 ? position : 0;
         this.renderer.setStyle(this.el.nativeElement, 'top', `${position}px`);
         this.renderer.setStyle(this.el.nativeElement, 'max-height', `${e.data.screen}px`);
