4066711d3a3730ad2673f2a11da9f2ac1507f12b
Angelo Crincoli <angelocrincoli91@gmail.com>
2023-02-23 05:17:50 -0400

    Agregando popup pozo

--- loteriaNacionalServer2/psdLoteria/test3.js
+++ loteriaNacionalServer2/psdLoteria/test3.js
@@ -1,7 +1,7 @@
 const fs = require("fs").promises;
 const auth = require("./auth");
 const juegos = require("./juegos");
-const config = require("../environments/test");
+const config = require("../environments/production");
 function timeout(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
