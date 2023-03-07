27bdbd4687104dbfa29b5954b1dfb9d8f8ba43ad
Angelo Crincoli <angelocrincoli91@gmail.com>
2023-03-07 01:53:10 -0400

    Actualizando vista de admin

--- loteriaNacionalServer2/admin/adminServer.js
+++ loteriaNacionalServer2/admin/adminServer.js
@@ -36,8 +36,9 @@ module.exports.init = function (port) {
   });
 
   // Body Parser Middleware
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ limit: "50mb" }));
   app.use(bodyParser.json());
-
   //Adding routes
   app.use("/", routes);
   app.use("/uploads", express.static(config.adminUploadsPath));
