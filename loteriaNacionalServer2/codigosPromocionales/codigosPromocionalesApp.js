3c125b18d66339f7a061b877ad90619e01cf2c3d
Angelo Crincoli <angelocrincoli91@gmail.com>
2023-03-10 13:04:17 -0400

    Preraparando despliegue de funcionalidad de popup

--- loteriaNacionalServer2/codigosPromocionales/codigosPromocionalesApp.js
+++ loteriaNacionalServer2/codigosPromocionales/codigosPromocionalesApp.js
@@ -1,5 +1,5 @@
 const codigosPromocionalesServer = require("./codigosPromocionalesServer");
-const config = require("../environments/test");
+const config = require("../environments/production");
 const cronjobs = require("./cronjobs");
 
 const codigosPromocionalesPort = config.codigosPromocionalesPort;
