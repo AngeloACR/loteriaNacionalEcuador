2218b5d472f1bb269d86ff5395f9cc4fe7fe0dc3
Angelo Crincoli <angelocrincoli91@gmail.com>
2023-03-15 14:48:53 -0400

    Resolviendo bug de ventas

--- loteriaNacionalServer2/ventas/controller/main.js
+++ loteriaNacionalServer2/ventas/controller/main.js
@@ -503,6 +503,7 @@ const ventasController = {
         loteriaVentaResponse.ticketId,
         ip
       ); */
+      let codigoPromocionalResponse = "";
       /* RESPUESTA DE API */
       let finalResponse = {
         data: apiVentaResponse,
