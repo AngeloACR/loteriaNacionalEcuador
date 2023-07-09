81c26deaac57fa986624be39c2c4789a34102f8f
Angelo Crincoli <angelocrincoli91@gmail.com>
2023-07-09 07:06:57 -0400

    Corrigiendo error en server de uploads

--- loteriaNacionalServer2/uploads/routes/main.js
+++ loteriaNacionalServer2/uploads/routes/main.js
@@ -4,3 +4,4 @@ const mainController = require("../controller/main");
 
 lotteryRouter.get("/", mainController.getNewFiles);
 lotteryRouter.get("/pega3", mainController.getPega3);
+module.exports = lotteryRouter;
