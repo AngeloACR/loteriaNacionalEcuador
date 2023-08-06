be00adcaac21b90e3687f73f0906b40dbe9cda90
Angelo Crincoli <angelocrincoli91@gmail.com>
2023-08-06 10:44:35 -0400

    Preparando deploy

--- loteriaNacionalServer2/ventas/controller/main.js
+++ loteriaNacionalServer2/ventas/controller/main.js
@@ -506,13 +506,13 @@ const ventasController = {
         );
       }
       /* GENERAR CODIGOS PROMOCIONALES */
-      let codigoPromocionalResponse = await CodigosPromocionales.setCode(
+      /*let codigoPromocionalResponse = await CodigosPromocionales.setCode(
         parseFloat(totalVenta),
         personaId,
         loteriaVentaResponse.ticketId,
         ip
-      );
-      //let codigoPromocionalResponse = "";
+      );*/
+      let codigoPromocionalResponse = "";
       /* RESPUESTA DE API */
       let finalResponse = {
         data: apiVentaResponse,
